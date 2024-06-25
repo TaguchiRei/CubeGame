8f035cda4276dee61c2b2c8ce5b1a40ae27961fa
TaguchiRei <ren.len2426@gmail.com>
2024-06-25 11:55:18 +0900

    レバースイッチの作業続き

--- Assets/SwitchBox.cs
+++ Assets/SwitchBox.cs
@@ -6,6 +6,8 @@ public class SwitchBox : MonoBehaviour
     public float X = 0;
     public float Y = 0;
     bool _lever = false;
+    bool _keyDownWait = false;
+    bool _canUse =false;
     void Start()
     {
         circleCollider = GetComponent<CircleCollider2D>();
@@ -25,15 +27,37 @@ public class SwitchBox : MonoBehaviour
             {
                 circleCollider.enabled = false;
             }
+        }
+        else if (GameMaster._gameMode == 0)
+        {
             Vector2 select = new Vector2(GameMaster._mousePositionX, GameMaster._mousePositionY);
             Vector2 thisPosition = transform.position;
-            if (Input.GetKeyDown(KeyCode.R) && select == thisPosition && GameMaster.key)
+            if (Input.GetKeyDown(KeyCode.R))
             {
-
+                if (select == thisPosition && _keyDownWait == false)
+                {
+                    _keyDownWait = true;
+                }else if (_keyDownWait)
+                {
+                    X = GameMaster._mousePositionX;
+                    Y = GameMaster._mousePositionY;
+                    _keyDownWait = false;
+                }
             }
-
         }
-
-
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            _canUse = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            _canUse = false;
+        }
     }
 }
